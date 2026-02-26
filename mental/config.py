92566486b54413ff598922ead8dc9eb594b2240f
Thu Ngo <160973994+Clementine27@users.noreply.github.com>
2026-02-26 12:42:29 -0500

    final messgse extraction

---
+++ mental/config.py
@@ -0,0 +1,17 @@
+# PATHS 
+
+# everything else 
+RAW_DIR = "data/0.raw"
+# the html files 
+INTERIM_DIR = "data/1.interim"
+# the json files 
+PROCESSED_DIR = "data/2.processed"
+# final csv files 
+FINAL_DIR = "data/3.final"
+
+FINAL_DIR_2 = "data/3.final_v2"
+
+# KEYWORDS
+DATASET = "data_3.csv"
+DATA_KEYWORD = "window.__reactRouterContext.streamController.enqueue"
+CHAT_GPT_QUESTION = "LLM_S_2"
